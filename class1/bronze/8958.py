bccb9752b4094d02ee719528b195dcdf07901e4f
ChoiseU <csw980928@gmail.com>
2024-09-03 13:42:11 +0900

    feat: OX 퀴즈

--- class1/bronze/8958.py
+++ class1/bronze/8958.py
@@ -1,11 +1,21 @@
-# map 선언
-dupl = {}
+# 테스트 케이스 갯수
+cnt = int(input())
 
-# 10개의 숫자를 입력 받는다
-for i in range(10):
-    # 이를 42로 나눈 나머지를 구한다.
-    num = int(input()) % 42
-    dupl[num] = 1
+# 테스트 갯수만큼 반복
+for _ in range(cnt):
+    # 테스트 데이터 입력받기
+    lines = input()
 
-# 서로 다른 값이 몇 개 가 있는지 출력한다.
-print(len(dupl))
+    # 점수와 몇 개 연속인지 체크하는 변수 선언
+    score = 0
+    cnt = 0
+
+    # 반복 돌려서 O가 중복되는 지 확인 후 최종 값에 더하기
+    for char in lines:
+        if char == "O":
+            cnt += 1
+            score += cnt
+        else:    
+            cnt = 0
+
+    print(score)
